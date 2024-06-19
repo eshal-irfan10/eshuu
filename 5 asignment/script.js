d35d808dc8d225acb951f7a0e2c3bac80699bfa8
Eshal Irfan <155638363+eshal-irfan10@users.noreply.github.com>
2024-06-19 05:41:13 -0700

    Add files via upload

---
+++ 5 asignment/script.js
@@ -0,0 +1,121 @@
+// let sum = 3;
+// let num = 5;
+// let add = 3 + 5;
+// document.write(add)
+// // 2    
+// let sub = 3;
+// let subt = 5;
+// let subtract = 3 - 5;
+// document.write(subtract);
+// // 3
+// let multiply = 3;
+// let multi = 5;
+// let multipication = 3 * 5;
+// document.write(multipication);
+// // 4
+// let divide = 3;
+// let divi = 5;
+// let division = 3 / 5;
+// document.write(division);
+// // 5
+// let module = 3;
+// let modules = 5;
+// let modulas = 3 % 5;
+// document.write(modulas) ;
+// Q3
+// let e = 5;
+// let a = ++e;
+// console.log(a);
+// let v = 6 + 7;
+// console.log(v);
+// let w = 13 ;
+// let t = 13 - 1;
+// console.log(t);
+// let q = 12 / 3;
+// console.log(q) + "<br>";
+
+// document.write("Value after variable declaration is undefined <br>");
+// document.write("Initial value: 5 <br>")
+// document.write("Value after increment is: 6 <br>");
+// document.write("Value after addition is: 13 <br>");
+// document.write("Value after  decrement is: 12 <br>");
+// document.write("The reminder  is: 0 <br>");
+
+
+
+// let es = 600 + 600 + 600 + 600 + 600;
+// console.log(es);
+// document.write("Total cost to buy 5 tickets to a movie is 3000PKR");
+
+
+// // Convert Celsius to Fahrenheit
+// let celsius = 25;
+// let fahrenheit = celsius * 9/5 + 32;
+// console.log(`${celsius}°C is ${fahrenheit}°F`);
+
+// // Convert Fahrenheit to Celsius
+// fahrenheit = 77.0;
+// celsius = (fahrenheit - 32) * 5/9;
+// console.log(`${fahrenheit}°F is ${celsius}°C`);
+
+// document.write("25°C is 77°F <br>");
+// document.write("70°F is 21.111111111111111°C");
+// let item1 = 650;
+// document.write(" Price of item1 " + " is " + item1 + "<br>");
+// let quantity1 = 1;
+// document.write(" Quantity of item1 is " + quantity1 + "<br>");
+// let item2 = 100;
+// document.write(" Price of item1 " + " is " + item2 + "<br>");
+// let quantity2 = 7;
+// document.write(" Quantity of item1 is " + quantity2 + "<br>");
+// let charges = 100;
+// document.write(" Shipping Charges " + charges + "<br>");
+// let Total = 650 + 100 + 100  ;
+// document.write(" Total cost of your order is " + Total);
+
+// let total = 980;
+// document.write("Total marks: " + total + "<br>")
+// let obtained = 804;
+// document.write("Marks obtained: " + obtained + "<br>")
+// let percent = obtained / + total *+ 100;
+// document.write("Percentage: " + percent);
+
+
+// let us = 10 * 104.80;
+// console.log(us);
+// let riyal = 25 * 28;
+// console.log(riyal);
+// let _total = 1048 + 700
+// document.write("Total currency is PKR: " + _total);
+// let num2 = 17;
+// console.log(num);
+// let addd = num + 5;
+// console.log(add);
+// let multiply_ = add * 10;
+// console.log(multiply);
+// let divide$ = multiply / 2;
+// console.log(divide);
+// let year = 2024;
+// document.write("Current year: " + year + "<br>");
+// let years = 2013;
+// document.write("Birth year: " + years + "<br>");
+// let _year = 2024 - 2013;
+// document.write("Your age: " + _year);
+// for(let i=130; i > 10; i = i - 10){
+//     console.log("Table of 10");
+//     console.log(i);
+// }
+// Variables
+let favoriteSnack = "Pizza and Ice cream";
+let currentAge = 10;
+let maximumAge = 80;
+let amountPerDay = 1;
+
+// Calculation
+let totalSnacks = maximumAge - currentAge - amountPerDay;
+document.write("<h1>The Lifetime Supply Calculater <br> <br>");
+document.write("Favourite snacks : " + favoriteSnack   + "<br>");
+document.write("Current Age : " + currentAge +"<br>");
+document.write("Maximum Age : " + maximumAge + "<br>");
+document.write("Estimated Maximum Age  : " + amountPerDay + "<br>");
+document.write("You will need " + totalSnacks + " " + favoriteSnack + " to last you until the ripe old age of " + maximumAge);
